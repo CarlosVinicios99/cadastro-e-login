65a977e946e7ad288638f4349b1be03534182c44
Carlos Vinicios De Souza Azeredo <carlosvinicios@id.uff.br>
2024-07-22 00:00:11 -0300

    feature: criação da rota de login

--- back-end/register/src/main/java/carpelune/auth/controllers/AuthController.java
+++ back-end/register/src/main/java/carpelune/auth/controllers/AuthController.java
@@ -1,9 +1,14 @@
 package carpelune.auth.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import carpelune.auth.dto.LoginRequestDTO;
+import carpelune.auth.dto.LoginResponseDTO;
 import carpelune.auth.services.AuthService;
 
 @RestController
@@ -12,5 +17,11 @@ public class AuthController {
 	
 	@Autowired
 	private AuthService authService;
+	
+	
+	@PostMapping("/login")
+	public ResponseEntity<LoginResponseDTO> login(@RequestBody LoginRequestDTO loginRequestDto){
+		return this.authService.login(loginRequestDto);
+	}
 	
 }
