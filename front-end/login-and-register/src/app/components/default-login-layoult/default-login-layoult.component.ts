8b316f725ad4508a5b3f2af26369f4cb0aadadc1
Carlos Vinicios De Souza Azeredo <carlosvinicios@id.uff.br>
2024-07-24 00:12:03 -0300

    feature: estilização inicial da tela de login e distribuíção dos elementos na tela

--- front-end/login-and-register/src/app/components/default-login-layoult/default-login-layoult.component.ts
+++ front-end/login-and-register/src/app/components/default-login-layoult/default-login-layoult.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-default-login-layoult',
@@ -8,5 +8,7 @@ import { Component } from '@angular/core';
   styleUrl: './default-login-layoult.component.scss'
 })
 export class DefaultLoginLayoultComponent {
-
+  @Input() title: string = ""
+  @Input() primaryBtnText = ""
+  @Input() secundaryBtnText = ""
 }
