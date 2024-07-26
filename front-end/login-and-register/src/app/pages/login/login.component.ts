b7d8753b2a92f62a287a791b7c99224a1e78dae5
Carlos Vinicios De Souza Azeredo <carlosvinicios@id.uff.br>
2024-07-25 22:56:51 -0300

    feature: definição do formulário

--- front-end/login-and-register/src/app/pages/login/login.component.ts
+++ front-end/login-and-register/src/app/pages/login/login.component.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
 import { DefaultLoginLayoultComponent } from '../../components/default-login-layoult/default-login-layoult.component';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [DefaultLoginLayoultComponent],
+  imports: [
+    DefaultLoginLayoultComponent,
+    ReactiveFormsModule
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+
+  loginForm!: FormGroup
+
+  constructor(){
+    this.loginForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required, Validators.minLength(6)])
+    })
+  }
 
 }
